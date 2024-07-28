12a1921e93d2ff4677335543a3f11e5d07d24ca3
bouaik <bouaik.lhou@gmail.com>
2024-07-28 19:23:04 +0100

    connect, read, show  user inputs

--- main.go
+++ main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
 
-	fmt.Println("hello, world!")
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+		fmt.Println("New client connected")
+		go handleClient(conn)
+	}
+}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Printf("Received: %s\n", buffer[:n])
+
+		_, err = conn.Write(buffer[:n])
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	}
 }
